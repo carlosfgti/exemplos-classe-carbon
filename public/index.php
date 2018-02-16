f578557bdc3a0db8e97b11c1f7fe38e7106a6f1d
Carlos Ferreira <carlos@especializati.com.br>
2018-02-16 10:36:45 -0200

    First exemples

--- public/index.php
+++ public/index.php
@@ -5,4 +5,21 @@ include __DIR__ . '/../bootstrap/start.php';
 
 use Carbon\Carbon;
 
-echo Carbon::now();
+// Exiber o timestamps atual (data e hora)
+// echo Carbon::now();
+
+
+// Exibir o timestamps de horas atrás
+// $hours = -6;
+// echo Carbon::now($hours);
+// echo Carbon::now(2); // 2 horas após o horário atual
+
+// Exibir outro timezone
+// echo Carbon::now(new DateTimeZone('Europe/London'));
+
+// Exibir data de amanhã
+//echo Carbon::tomorrow();
+
+// Exibir data de antes de ontem
+// echo Carbon::yesterday();
+
